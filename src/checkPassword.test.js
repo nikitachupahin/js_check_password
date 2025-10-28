19bec9b0cd9c1a6c68c742ed7c990bdfa250674f
Nikita Chupahin <nik123chupahin@gmail.com>
2025-10-28 12:58:02 +0200

    added tests

--- src/checkPassword.test.js
+++ src/checkPassword.test.js
@@ -8,12 +8,52 @@ describe(`Function 'checkPassword':`, () => {
   });
 
   it(`should return boolean`, () => {
-
+    expect(typeof checkPassword('Password1!')).toBe('boolean');
+    expect(typeof checkPassword('qwerty')).toBe('boolean');
   });
 
-  it(`should return 'true' for the valid password with 8 characters`, () => {
-
+  it(`should return 'true' for the valid password
+    with all rules`, () => {
+    expect(checkPassword('Password1!')).toBe(true);
   });
 
-  // write more tests here
+  it(`should return 'true' for the valid password
+    with at least 8 characters`, () => {
+    expect(checkPassword('Password1!')).toBe(true);
+  });
+
+  it(`should return 'true' for the valid password
+    with max 16 characters`, () => {
+    expect(checkPassword('Password1!Passwo')).toBe(true);
+  });
+
+  it(`should return 'false' for the invalid password
+    with less than 8 characters`, () => {
+    expect(checkPassword('Passw1!')).toBe(false);
+  });
+
+  it(`should return 'false' for the invalid password
+    with more than 16 characters`, () => {
+    expect(checkPassword('Password1!Password')).toBe(false);
+  });
+
+  it(`should return 'false' for the invalid password
+    without at least 1 digit`, () => {
+    expect(checkPassword('Str@ngks')).toBe(false);
+  });
+
+  it(`should return 'false' for the invalid password
+    without at least 1 special character`, () => {
+    expect(checkPassword('Qwerty123')).toBe(false);
+  });
+
+  it(`should return 'false' for the invalid password
+    without at least 1 uppercase letter`, () => {
+    expect(checkPassword('qwerty12@')).toBe(false);
+  });
+
+  it(`should return 'false' for the invalid password
+    without letters of the Latin alphabet `, () => {
+    expect(checkPassword('Пароль1!')).toBe(false);
+  });
 });
